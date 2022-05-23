5548c6012dd3f5165126d1fcfcee526c641d5e78
DhanteyUD <otseclinton60@gmail.com>
2022-05-23 11:10:11 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
 });
 
 // Server connection
-const PORT = process.env.PORT || 3050;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
